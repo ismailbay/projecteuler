5a283e68f06f228dfecca36a53c5d64d0a282f5c
Ismail Bay <ismail.bay@openforce.com>
2014-01-21 14:32:35 +0100

    added a very imperativ solution for problem 2

---
+++ scala/2.scala
@@ -0,0 +1,24 @@
+/** Problem 2
+  * 
+  * sequence starts with: 1, 2, 3, 5, 8, ...
+  *
+  * By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+  *
+  */ 
+object Euler2 {
+  def main(args: Array[String]) {
+    var fib1 = 1
+    var fib2 = 2
+
+    var fibList = List(fib1, fib2);
+
+    while (fib1 + fib2 < 4000000) {
+      fibList = fibList :+ (fib1 + fib2)
+      fib1 = fib2
+      fib2 = fibList.last
+    }
+
+    fibList = fibList filter (n => n % 2 == 0)
+    println(fibList.reduce(_ + _))
+  }                             
+}
