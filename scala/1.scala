45e5bd0ebea2123810936005035ef09eb657efc3
Ismail Bay <ismail.bay@openforce.com>
2014-01-21 14:41:58 +0100

    switched to sum (from reduce)

--- scala/1.scala
+++ scala/1.scala
@@ -5,6 +5,6 @@
   */
 object Euler1 {
   def main(args: Array[String]) {
-    println((0 until 1000) filter(n => (n % 3 == 0 || n % 5 == 0)) reduce(_ + _))
+    println((0 until 1000).filter(n => n % 3 == 0 || n % 5 == 0).sum)
   }
 }
